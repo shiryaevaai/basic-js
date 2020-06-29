8a8eafdd8292259ee2d0625d27c6e377002f6567
Anna Shiriaeva1 <anna_shiriaeva1@epam.com>
2020-06-29 18:53:06 +0400

    feat: implement carbon-dating function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,8 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  // throw new CustomError('Not implemented');
-  // remove line with error and write your code here
     let counter = 0;
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[0].length; j++) {
